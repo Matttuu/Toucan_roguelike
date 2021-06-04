9d22db422dfdf37436ea18cc60c8f88e025089b9
Matttuu <xuniqh@gmail.com>
2021-06-04 15:44:19 +0200

    Added Comments / Explanation

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,16 @@ use rltk::{Rltk, GameState}; // Require "Rltk" & "GameState" types from the name
 
 struct State{} // New Structure. (Class)
 impl GameState for State { // Implement trait "GameState" to "State". (Interface)
-    fn tick(&mut self, ctx : &mut Rltk) { // Void function
-        ctx.cls();
-        ctx.print(1, 1, "Hello Toucan World");
+    fn tick(&mut self, ctx : &mut Rltk) { // Void function // Pass in variable called 'ctx' // '&' Pass a reference (pointer to existing copy of the variable)
+        ctx.cls(); // Call function provided by ctx-variable. // This clears the screen
+        ctx.print(1, 1, "Hello Toucan World"); // Prints string at location (1,1)
     }
 }
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
-    let context = RltkBuilder::simple80x50()
+    let context = RltkBuilder::simple80x50() // Builder
         .with_title("Roguelike Toucan World")
         .build()?;
-    let gs = State {};
-    rltk::main_loop(context, gs)
+    let gs = State {}; // "Variable assigning" by making new variable called 'gs' and copying the 'State' struct above.
+    rltk::main_loop(context, gs)  // Game loop
 }
