2d02333315bc69e2446164683a8fa0f56b28ab5a
Matttuu <xuniqh@gmail.com>
2021-06-04 13:26:10 +0200

    Game loop added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk, GameState}; // Require "Rltk" & "GameState" types from the namespace "rltk".
+
+struct State{} // New Structure. (Class)
+impl GameState for State { // Implement trait "GameState" to "State". (Interface)
+    fn tick(&mut self, ctx : &mut Rltk) { // Void function
+        ctx.cls();
+        ctx.print(1, 1, "Hello Toucan World");
+    }
+}
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("Roguelike Toucan World")
+        .build()?;
+    let gs = State {};
+    rltk::main_loop(context, gs)
 }
